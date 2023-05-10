f64063b50bfb4c2fb81edbb089e1ec3e2ae115b4
isnanw <isnanwahyudi4@gmail.com>
2023-05-10 22:51:03 +0700

    breadcrumb

--- application/views/backend/_partials/breadcrumb.php
+++ application/views/backend/_partials/breadcrumb.php
@@ -6,7 +6,7 @@
                 <h4 class="fw-semibold mb-8"><?= $title; ?></h4>
                 <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a class="text-muted" href="#">Master</a></li>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#"><?= $title0; ?></a></li>
                     <li class="breadcrumb-item" aria-current="page"><?= $title; ?></li>
                 </ol>
                 </nav>
